57b876984c4aec47c7f0609e55013207de47ab72
Kiryl Piatrouski <zagubenko.kirill@gmail.com>
2026-06-05 13:48:55 +0300

    Remove generated artifacts from git tracking

--- apps/backend/src/main.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-console.log('Digital City OS Backend starting...');
-//# sourceMappingURL=main.js.map
--- apps/frontend/src/pages/index.js
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-    return (<main>
-      <h1>Digital City OS</h1>
-      <p>Loading...</p>
-    </main>);
-}
-//# sourceMappingURL=index.js.map
